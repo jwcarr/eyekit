46611e81379cdd613dc1250b607ed9565fcdfaf7
Jon Carr <jwcarr@users.noreply.github.com>
2020-09-06 18:03:37 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
 	name='eyekit',
-	version='0.0.13',
+	version='0.1.0',
 	author='Jon Carr',
 	author_email='jcarr@sissa.it',
 	description='A lite Python package for handling and visualizing eyetracking data',
@@ -20,5 +20,5 @@ setuptools.setup(
 		'Operating System :: OS Independent',
 	],
 	python_requires='>=3.6',
-	install_requires=['numpy>=1.13']
+	install_requires=['numpy>=1.13', 'cairosvg>=2.4']
 )
