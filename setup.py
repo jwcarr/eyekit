048003e1662c46fc72a3a7b2b1aa72b162973e77
Jon Carr <jwcarr@users.noreply.github.com>
2020-09-06 20:45:27 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
 	name='eyekit',
-	version='0.1.1',
+	version='0.1.2',
 	author='Jon Carr',
 	author_email='jcarr@sissa.it',
 	description='A lite Python package for handling and visualizing eyetracking data',
