df4136f91f68352dd975704980627c9471a708f3
Jon Carr <jwcarr@users.noreply.github.com>
2020-09-07 13:21:05 +0200

    Changed the way Characters inherit from parent Text

--- eyekit/text.py
+++ eyekit/text.py
@@ -3,23 +3,23 @@ import numpy as _np
 
 
 CASE_SENSITIVE = False
-ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÈÉÌÍÒÓÙÚabcdefghijklmnopqrstuvwxyzàáèéìíòóùú’ ')
-SPECIAL_CHARACTERS = {'’':' ', 'À':'A', 'Á':'A', 'È':'E', 'É':'E', 'Ì':'I', 'Í':'I', 'Ò':'O', 'Ó':'O', 'Ù':'U', 'Ú':'U', 'à':'a', 'á':'a', 'è':'e', 'é':'e', 'ì':'i', 'í':'i', 'ò':'o', 'ó':'o', 'ù':'u', 'ú':'u'}
+ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÈÉÌÍÒÓÙÚabcdefghijklmnopqrstuvwxyzàáèéìíòóùú')
+SPECIAL_CHARACTERS = {'À':'A', 'Á':'A', 'È':'E', 'É':'E', 'Ì':'I', 'Í':'I', 'Ò':'O', 'Ó':'O', 'Ù':'U', 'Ú':'U', 'à':'a', 'á':'a', 'è':'e', 'é':'e', 'ì':'i', 'í':'i', 'ò':'o', 'ó':'o', 'ù':'u', 'ú':'u'}
 IA_REGEX = _re.compile(r'(\[(.+?)\]\{(.+?)\})')
 
 
 class Character:
 
-	def __init__(self, char, r, c, x, y):
+	def __init__(self, parent_text, char, r, c):
+		self._parent_text = parent_text
 		self.char = char
+		self.r, self.c = r, c
 		if self.char in SPECIAL_CHARACTERS:
 			self.underlying_char = SPECIAL_CHARACTERS[char]
 		else:
 			self.underlying_char = char
 		if not CASE_SENSITIVE:
 			self.underlying_char = self.underlying_char.lower()
-		self.x, self.y = x, y
-		self.r, self.c = r, c
 
 	def __str__(self):
 		return self.char
@@ -39,6 +39,14 @@ class Character:
 		return self.underlying_char == other.lower()
 
 	@property
+	def x(self):
+		return self._parent_text.first_character_position[0] + self.c * self._parent_text.character_spacing
+
+	@property
+	def y(self):
+		return self._parent_text.first_character_position[1] + self.r * self._parent_text.line_spacing
+
+	@property
 	def xy(self):
 		return self.x, self.y
 	
@@ -47,7 +55,7 @@ class Character:
 		return self.r, self.c
 
 	@property
-	def ignore(self):
+	def non_word_character(self):
 		return self.char not in ALPHABET
 
 
@@ -100,11 +108,11 @@ class Text:
 		else:
 			raise ValueError('text should be a string or a list of strings')
 
-		self.characters = self._extract_characters()
-		self.n_rows = len(self.characters)
-		self.n_cols = max([len(row) for row in self.characters])
-		self.line_positions = _np.array([line[0].y for line in self.characters], dtype=int)
 		self._interest_areas = self._parse_interest_areas()
+		self._characters = self._extract_characters()
+		self.n_rows = len(self._characters)
+		self.n_cols = max([len(row) for row in self._characters])
+		self.line_positions = _np.array([line[0].y for line in self._characters], dtype=int)
 
 	def __repr__(self):
 		return 'Text[%s...]' % ''.join(self.text[0][:16])
@@ -120,7 +128,7 @@ class Text:
 		x = self.first_character_position[0] + c*self.character_spacing
 		y = self.first_character_position[1] + r*self.line_spacing
 		try:
-			return self.characters[r][c], (x, y)
+			return self._characters[r][c], (x, y)
 		except IndexError:
 			return None, (x, y)
 
@@ -160,9 +168,7 @@ class Text:
 		for r, line in enumerate(self.text):
 			characters_line = []
 			for c, char in enumerate(line):
-				x = self.first_character_position[0] + c*self.character_spacing
-				y = self.first_character_position[1] + r*self.line_spacing
-				character = Character(char, r, c, x, y)
+				character = Character(self, char, r, c)
 				characters_line.append(character)
 			characters.append(characters_line)
 		return characters
@@ -216,7 +222,7 @@ class Text:
 		Returns True if the given fixation is within a certain threshold of
 		any character in the text. Returns False otherwise.
 		'''
-		for line in self.characters:
+		for line in self._characters:
 			for char in line:
 				if distance(fixation.xy, char.xy) <= in_bounds_threshold:
 					return True
@@ -226,28 +232,27 @@ class Text:
 		'''
 		Iterate over lines in the text.
 		'''
-		for line in self.characters:
+		for line in self._characters:
 			yield line
 
-	def iter_words(self, filter_func=None, line_n=None):
+	def words(self):
 		'''
-		Iterate over words in the text, optionally on a given line.
+		Iterate over words in the text.
 		'''
+		word_i = 0
 		word = []
-		for i, line in enumerate(self.characters):
-			if line_n is not None and i != line_n:
-				continue
+		for r, line in enumerate(self._characters):
 			for char in line:
-				if char.ignore:
-					continue
-				if str(char) == ' ':
-					if filter_func is None or filter_func(word):
-						yield word
+				if char.non_word_character:
+					if word:
+						yield InterestArea(self, word[0].r, word[0].c, len(word), 'word_%i'%word_i)
+						word_i += 1
 					word = []
 				else:
 					word.append(char)
-			if filter_func is None or filter_func(word):
-				yield word
+			if word:
+				yield InterestArea(self, word[0].r, word[0].c, len(word), 'word_%i'%word_i)
+				word_i += 1
 			word = []
 
 	def get_word(self, label):
@@ -260,7 +265,7 @@ class Text:
 		'''
 		Iterate over characters in the text, optionally on a given line.
 		'''
-		for i, line in enumerate(self.characters):
+		for i, line in enumerate(self._characters):
 			if line_n is not None and i != line_n:
 				continue
 			for char in line:
@@ -273,7 +278,7 @@ class Text:
 		'''
 		Iterate over ngrams in the text, optionally on a given line.
 		'''
-		for i, line in enumerate(self.characters):
+		for i, line in enumerate(self._characters):
 			if line_n is not None and i != line_n:
 				continue
 			for j in range(len(line)-(n-1)):
