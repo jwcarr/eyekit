91295f3a08031df61a174ff6bec5911af9fdc0e3
Jon Carr <jwcarr@users.noreply.github.com>
2020-09-26 15:16:09 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as file:
 
 setuptools.setup(
 	name='eyekit',
-	version='0.2.0',
+	version='0.2.1',
 	author='Jon Carr',
 	author_email='jcarr@sissa.it',
 	description='A Python package for doing open, reproducible science on reading behavior',
