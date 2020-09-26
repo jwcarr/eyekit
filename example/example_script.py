4a45f45f4549d035af1b4f047a58e10772aa1f19
Jon Carr <jwcarr@users.noreply.github.com>
2020-09-26 11:14:16 +0200

    Fixed example script

--- example/example_script.py
+++ example/example_script.py
@@ -12,9 +12,10 @@ data = eyekit.io.read('example_data.json')
 texts = eyekit.io.read('example_texts.json')
 
 for trial_id, trial in data.items():
+	seq = trial['fixations']
 	passage_id = trial['passage_id']
 	txt = texts[passage_id]['text']
 	img = eyekit.Image(1920, 1080)
-	img.render_text(txt)
-	img.render_fixations(trial['fixations'])
+	img.draw_text_block(txt)
+	img.draw_fixation_sequence(seq)
 	img.save(f'{trial_id}.pdf')
