a54895415a947d2f11fcb7a7f662e969404662f6
Jon Carr <jwcarr@users.noreply.github.com>
2021-02-24 01:44:26 +0100

    More tests

--- tests/test_text.py
+++ tests/test_text.py
@@ -31,6 +31,7 @@ def test_initialization():
     assert txt.align == "left"
     assert txt.anchor == "left"
     assert txt.alphabet is None
+    assert txt.autopad == True
     assert txt.n_rows == 1
     assert txt.n_cols == 45
     assert len(txt) == 45
@@ -43,6 +44,14 @@ def test_zone_extraction():
         assert zone.text in ["jump", "ed"]
         assert zone.baseline == 500
         assert zone.height == 36
+
+
+def test_zone_find():
+    assert txt["stem_1"].text == "jump"
+    assert txt["suffix_1"].text == "ed"
+    txt[0:4:19].id = "test_id"
+    assert txt["test_id"].text == "quick brown fox"
+    assert txt[0:4:19].id == "test_id"
 
 
 def test_word_extraction():
@@ -65,9 +74,19 @@ def test_word_extraction():
 
 def test_arbitrary_extraction():
     assert txt[0:0:3].text == "The"
-    assert txt[0:41:45].text == "dog."
     assert txt["0:0:3"].text == "The"
+    assert txt[(0, 0, 3)].text == "The"
+    assert txt[0:41:45].text == "dog."
     assert txt["0:41:45"].text == "dog."
+    assert txt[(0, 41, 45)].text == "dog."
+    assert txt[0:4:19].text == "quick brown fox"
+    assert txt["0:4:19"].text == "quick brown fox"
+    assert txt[(0, 4, 19)].text == "quick brown fox"
+
+
+def test_IA_location():
+    assert txt[0:0:3].location == (0, 0, 3)
+    assert txt[0:5:40].location == (0, 5, 40)
 
 
 def test_which_methods():
@@ -113,6 +132,7 @@ def test_serialize():
     assert data["align"] == "left"
     assert data["anchor"] == "left"
     assert data["alphabet"] is None
+    assert data["autopad"] == True
 
 
 def test_Font():
