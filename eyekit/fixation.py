32212bb21102487b18808709afc6ee7b15ab4159
Jon Carr <4412666+jwcarr@users.noreply.github.com>
2021-11-11 00:55:15 +0100

    Add basic support for storing pupil size information

--- eyekit/fixation.py
+++ eyekit/fixation.py
@@ -13,7 +13,14 @@ class Fixation:
     """
 
     def __init__(
-        self, index: int, x: int, y: int, start: int, end: int, discarded: bool = False
+        self,
+        index: int,
+        x: int,
+        y: int,
+        start: int,
+        end: int,
+        pupil_size: int = None,
+        discarded: bool = False,
     ):
         if end < start:
             raise ValueError(
@@ -24,6 +31,11 @@ class Fixation:
         self.y = y
         self.start = start
         self.end = end
+        if pupil_size is True or pupil_size == "discarded":
+            # for backwards compatibility prior to 0.4
+            pupil_size = None
+            discarded = True
+        self.pupil_size = pupil_size
         self.discarded = discarded
 
     def __repr__(self):
@@ -74,6 +86,18 @@ class Fixation:
     def duration(self) -> int:
         """Duration of the fixation in milliseconds."""
         return self._end - self._start
+
+    @property
+    def pupil_size(self) -> int:
+        """Size of the pupil. `None` if no pupil size is recorded."""
+        return self._pupil_size
+
+    @pupil_size.setter
+    def pupil_size(self, pupil_size):
+        if pupil_size is None:
+            self._pupil_size = None
+        else:
+            self._pupil_size = int(pupil_size)
 
     @property
     def discarded(self) -> bool:
